d6cc88f03d132fc2b22cf20c471cb1ff6b43cd45
wariox3 <maestradaz3@gmail.com>
2026-03-27 16:35:10 -0500

    feat: adicionar cors path

--- app/main.py
+++ app/main.py
@@ -65,7 +65,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=["Content-Type", "Authorization", "Accept", "X-API-Key"]
 )
 
