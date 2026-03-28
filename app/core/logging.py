1d132c49b1c47f33fd8cbdfa86d845ef73f38272
wariox3 <maestradaz3@gmail.com>
2026-03-28 12:22:12 -0500

    feat: add sys.stdout stream to logging configuration

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,8 +1,10 @@
 import logging
+import sys
 
 def setup_logging():
 
     logging.basicConfig(
+        stream=sys.stdout,
         level=logging.INFO,
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s"
     )
